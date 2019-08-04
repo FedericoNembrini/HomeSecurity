f22f85f328e39df57008e31d781af1f279279c0f
FedericoNembrini <federico.nembrini.99@gmail.com>
2019-08-04 23:18:48 +0200

    Develop: - Fix. - Changed Sleep Time and Split Record Time.

--- HomeSecurityCamera/record.sh
+++ HomeSecurityCamera/record.sh
@@ -1,6 +1,6 @@
 # Takes Video Input from Url, Peform a Copy Only (Demux and Mix) of the Stream Into 1 Hours Video File.
 while :
 do
-    ffmpeg -r 24 -i rtsp://localhost:8554/ -c copy -map 0 -f segment -strftime 1 -segment_time 3600 -segment_format mp4 -r 24 /home/pi/HomeSecurityCamera/Recordings/%d-%m-%Y_%H-%M-%S.mp4
+    ffmpeg -r 24 -i rtsp://localhost:8554/ -c copy -map 0 -f segment -strftime 1 -segment_time 2700 -segment_format mp4 -r 24 /home/pi/HomeSecurityCamera/Recordings/%d-%m-%Y_%H-%M-%S.mp4
     sleep 3m
 done
