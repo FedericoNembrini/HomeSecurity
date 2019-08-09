d1b2394589105869255db1f62d5c232d6152bf00
FedericoNembrini <federico.nembrini.99@gmail.com>
2019-08-09 23:33:57 +0200

    assets: - Added Structure. - Added Images. - Deleted File.

--- HomeSecurityCamera/stream.sh
+++
@@ -1,6 +0,0 @@
-#Takes data from the device node specified on 1Megabyte Block and supply data to vlc video streaming.
-while :
-do
-    dd if=/dev/uv4l-camera bs=1M | cvlc stream:///dev/stdin --sout '#rtp{sdp=rtsp://:8554/}' --demux=h264 --h264-fps=24
-    sleep 3m
-done
